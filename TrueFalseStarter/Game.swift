8da82533e58bf568dad1e38f038e2a340608c38f
Scott Baumbich <sbaumbich777@gmail.com>
2016-08-22 07:40:04 -0500

    Game class added

---
+++ TrueFalseStarter/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  TrueFalseStarter
+//
+//  Created by Scott Baumbich on 8/22/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+class Game {
+
+    let questionsPerRound: Int
+    let lightningMode: Bool
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion: Int = 0
+    var gameSound: SystemSoundID = 0
+
+    
+}
